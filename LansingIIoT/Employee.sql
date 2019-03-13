9e640a2955e2ddc90aacf36992fe84dd83d584df
Jeff Phillips <jeff.phillips@orchid-ortho.com>
2019-03-13 15:01:30 -0400

    Added Employee table

---
+++ LansingIIoT/Employee.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Employee]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] NVARCHAR(100) NULL, 
+    [MiddleName] NVARCHAR(100) NULL, 
+    [LastName] NVARCHAR(100) NULL, 
+    [EmployeeNo] NVARCHAR(50) NULL, 
+    [EmployeeBadgeId] INT NULL
+)
