040d372ffab189f3e81d7ce743414c959f859654
Jeff Phillips <jeff.phillips@orchid-ortho.com>
2019-03-27 21:17:39 -0400

    Fixed primary keys

--- LansingIIoT/DaybyHour/WorkCenterGroupWorkCenter.sql
+++ LansingIIoT/DaybyHour/WorkCenterGroupWorkCenter.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbh].[WorkCenterGroupWorkCenter]
 (
-	[WorkCenterGroupId] INT NOT NULL PRIMARY KEY, 
-    [WorkCenterId] INT NOT NULL PRIMARY KEY, 
+	[WorkCenterGroupId] INT NOT NULL, 
+    [WorkCenterId] INT NOT NULL, 
+	PRIMARY KEY (WorkCenterId, WorkCenterGroupId),
     CONSTRAINT [FK_WorkCenterGroupWorkCenter_WorkCenterGroup] FOREIGN KEY ([WorkCenterGroupId]) REFERENCES [dbh].[WorkCenterGroup]([Id]), 
     CONSTRAINT [FK_WorkCenterGroupWorkCenter_WorkCenter] FOREIGN KEY ([WorkCenterId]) REFERENCES [dbh].[WorkCenter]([Id]),
 )
--- LansingIIoT/DaybyHour/WorkCenterReason.sql
+++ LansingIIoT/DaybyHour/WorkCenterReason.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [dbh].[WorkCenterReason]
 (
-	[WorkCenterId] INT NOT NULL PRIMARY KEY, 
-    [ReasonCodeId] INT NOT NULL PRIMARY KEY, 
+	[WorkCenterId] INT NOT NULL, 
+    [ReasonCodeId] INT NOT NULL, 
+	PRIMARY KEY (WorkCenterId, ReasonCodeId),
     CONSTRAINT [FK_WorkCenterReason_WorkCenter] FOREIGN KEY ([WorkCenterId]) REFERENCES [dbh].[WorkCenter]([Id]), 
     CONSTRAINT [FK_WorkCenterReason_ReasonCode] FOREIGN KEY ([ReasonCodeId]) REFERENCES [dbh].[ReasonCode]([Id])
 )
