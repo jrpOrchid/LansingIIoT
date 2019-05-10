d9c8a5edfd5f4446bfb3e664ad72b8b63e1d4817
Jeff Phillips <jeff.phillips@orchid-ortho.com>
2019-05-09 20:43:21 -0400

    Added work center setup step table

---
+++ LansingIIoT/Tables/WorkCenterSetupStep.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[WorkCenterSetupStep]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [WorkCenterId] INT NOT NULL, 
+    [Step] FLOAT NULL, 
+    [StepName] NVARCHAR(500) NULL, 
+    [Instructions] NVARCHAR(MAX) NULL, 
+    CONSTRAINT [FK_WorkCenterSetupStep_WorkCenter] FOREIGN KEY ([WorkCenterId]) REFERENCES [WorkCenter]([Id])
+)
