53a16f46439dbc5eacb00b9d8ff0470656caa48e
Jeff Phillips <jeff.phillips@orchid-ortho.com>
2019-06-22 10:47:38 -0400

    Added Logged By to AssetTimeLoss

--- LansingIIoT/Tables/AssetTimeLoss.sql
+++ LansingIIoT/Tables/AssetTimeLoss.sql
@@ -6,6 +6,8 @@
     [EndDateTime] DATETIME NOT NULL, 
     [TotalHours] FLOAT NULL, 
     [ReasonId] INT NULL, 
+    [LoggedByEmployeeId] INT NULL, 
     CONSTRAINT [FK_AssetTimeLoss_AvailabilityLossReason] FOREIGN KEY ([ReasonId]) REFERENCES [AvailabilityLossReason]([Id]), 
-    CONSTRAINT [FK_AssetTimeLoss_Asset] FOREIGN KEY ([AssetId]) REFERENCES [Asset]([Id])
+    CONSTRAINT [FK_AssetTimeLoss_Asset] FOREIGN KEY ([AssetId]) REFERENCES [Asset]([Id]), 
+    CONSTRAINT [FK_AssetTimeLoss_Employee] FOREIGN KEY ([LoggedByEmployeeId]) REFERENCES [Employee]([Id])
 )
